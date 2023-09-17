4601c4f0eaf4758d37ef65f8690b3fe6f8498791
Bill Maya <bill.maya@gmail.com>
2023-09-17 16:33:06 -0400

    Replaced old entity and attribute Item.timestamp with new entity and attribute Words.date_written to get project to build successfully.

--- one-forty-words/ContentView.swift
+++ one-forty-words/ContentView.swift
@@ -12,18 +12,18 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Words.date_written, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<Words>
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Item at \(item.date_written!, formatter: itemFormatter)")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text(item.date_written!, formatter: itemFormatter)
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -41,8 +41,8 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = Words(context: viewContext)
+            newItem.date_written = Date()
 
             do {
                 try viewContext.save()
